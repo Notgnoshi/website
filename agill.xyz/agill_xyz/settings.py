6e808bd158bb33cc29f3c0c80dd8b1e0e1c5986d
Austin Gill <notgnoshi@gmail.com>
2018-05-20 17:06:51 -0500

    Move static root so that it's not copied for each subdomain

--- agill.xyz/agill_xyz/settings.py
+++ agill.xyz/agill_xyz/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
-# Static site configuration taken from http://goodcode.io/articles/django-staticsite/
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,4 +73,5 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = False
 ROOT_URLCONF = 'agill_xyz.urls'
 WSGI_APPLICATION = 'agill_xyz.wsgi.application'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# path relative to manage.py
+STATIC_ROOT = str(Path('../shared_content/staticroot/').resolve())
--- research.agill.xyz/research_agill_xyz/settings.py
+++ research.agill.xyz/research_agill_xyz/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 # Static site configuration taken from http://goodcode.io/articles/django-staticsite/
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,4 +74,5 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = False
 ROOT_URLCONF = 'research_agill_xyz.urls'
 WSGI_APPLICATION = 'research_agill_xyz.wsgi.application'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# Path relative to manage.py
+STATIC_ROOT = str(Path('../shared_content/staticroot/').resolve())
