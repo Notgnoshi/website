2552fa4fb833db7c4ed8493ed12d72b53efa9b4f
Austin Gill <notgnoshi@gmail.com>
2020-01-03 18:36:56 -0700

    Fix nginx-proxy settings for plex container

--- scripts/docker/plex.sh
+++ scripts/docker/plex.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
-set -o errexit
-set -o nounset
-set -o pipefail
 
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do
-    # resolve $SOURCE until the file is no longer a symlink
-    DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
-    SOURCE="$(readlink "$SOURCE")"
-    # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
-done
-REPO_ROOT="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)/../../"
-REPO_ROOT="$(readlink -f "${REPO_ROOT}")"
-
-# Run the application container for a single subdomain.
-# See: https://hub.docker.com/_/nginx for documentation.
 docker run \
     --detach \
-    --name "nginx-plex" \
-    --mount "type=bind,source=${REPO_ROOT}/config/plex.conf,target=/etc/nginx/conf.d/default.conf,readonly" \
-    --mount "type=bind,source=${REPO_ROOT}/config/nginx.conf,target=/etc/nginx/nginx.conf,readonly" \
-    --env "VIRTUAL_HOST=plex.agill.xyz,plex.localhost" \
-    --env "LETSENCRYPT_HOST=plex.agill.xyz" \
-    --env "LETSENCRYPT_EMAIL=notgnoshi@gmail.com" \
     --restart unless-stopped \
-    nginx
+    --name plex \
+    --publish 32400:32400 \
+    --expose 32400 \
+    --env TZ="MDT" \
+    --env PLEX_UID=1000 \
+    --env PLEX_GID=1000 \
+    --env VIRTUAL_HOST=plex.agill.xyz,plex.localhost \
+    --env VIRTUAL_PORT=32400 \
+    --env LETSENCRYPT_HOST=plex.agill.xyz \
+    --env LETSENCRYPT_EMAIL=notgnoshi@gmail.com \
+    --mount "type=bind,source=/srv/plex/config,target=/config" \
+    --mount "type=bind,source=/srv/plex/transcode,target=/transcode" \
+    --mount "type=bind,source=/srv/plex/data,target=/data" \
+    plexinc/pms-docker:plexpass
+
+# TODO: Set PLEX_UID and PLEX_GID intelligently.
