184a10dc055a7ad6768cb3ab6c25164559177070
Austin Gill <notgnoshi@gmail.com>
2020-01-17 15:28:05 -0700

    Remove research subdomain config

--- scripts/docker/letsencrypt.sh
+++ scripts/docker/letsencrypt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 docker run \
-	--detach \
+    --detach \
     --name nginx-proxy-letsencrypt \
     --volumes-from nginx-proxy \
     --volume /var/run/docker.sock:/var/run/docker.sock:ro \
-    --env "DEBUG=true" \
     --env "DEFAULT_EMAIL=notgnoshi@gmail.com" \
     --restart unless-stopped \
     jrcs/letsencrypt-nginx-proxy-companion
+    # --env "DEBUG=true" \
