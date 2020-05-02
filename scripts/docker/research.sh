94b115bb91e5ddbfbddcbfaf37cdee81c4432b51
Austin Gill <Notgnoshi@gmail.com>
2020-05-02 16:42:22 +0000

    Add script for research API

---
+++ scripts/docker/research.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+docker run \
+    --interactive \
+    --tty \
+    --detach \
+    --restart unless-stopped \
+    --name research-api \
+    --mount "type=bind,source=/srv/research,target=/app" \
+    --workdir=/app \
+    --expose 80 \
+    --env WEB_CONCURRENCY=1 \
+    --env HTTPS_METHOD=nohttps \
+    --env VIRTUAL_HOST=api.localhost \
+    notgnoshi/api
