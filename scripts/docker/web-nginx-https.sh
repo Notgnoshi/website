ebe9be7bad32a11ac0811f24a142a6839b7071cf
Austin Gill <Notgnoshi@gmail.com>
2021-09-04 11:53:23 -0500

    Use named volumes to make certs persistent
    This means that the acme-companion container does not have to regenerate
    the certificates every time it starts up. This makes it less likely to
    hit the letsencrypt certificate rate limiting (five new certs a week).
    
    Additionally, use the official, maintained, images
    nginxproxy/nginx-proxy, and nginxproxy/acme-companion. These images
    originated from the ones that I was originally using, but are now owned
    and maintained by the same organization.
    
    Closes #91
    Closes #96
    Closes #97

--- scripts/docker/web-nginx-https.sh
+++ scripts/docker/web-nginx-https.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# See: https://github.com/nginx-proxy/acme-companion
 docker run \
     --detach \
-    --name nginx-proxy-letsencrypt \
+    --name nginx-proxy-acme \
     --volumes-from nginx-proxy \
     --volume /var/run/docker.sock:/var/run/docker.sock:ro \
+    --volume nginx_acme:/etc/acme.sh \
     --env "DEFAULT_EMAIL=notgnoshi@gmail.com" \
     --restart unless-stopped \
-    jrcs/letsencrypt-nginx-proxy-companion
+    nginxproxy/acme-companion
     # --env "DEBUG=true" \
